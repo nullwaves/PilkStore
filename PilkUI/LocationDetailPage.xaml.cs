d1112ddd58edf392be24d0b07ff5bec1f4c8b9ba
null <evanb96@gmail.com>
2024-01-26 02:42:20 -0600

    Warn when sub-locations will be deleted

--- PilkUI/LocationDetailPage.xaml.cs
+++ PilkUI/LocationDetailPage.xaml.cs
@@ -5,29 +5,33 @@ namespace PilkUI;
 
 public partial class LocationDetailPage : ContentPage
 {
-	public LocationDetailPage()
-	{
-		InitializeComponent();
-	}
+    public LocationDetailPage()
+    {
+        InitializeComponent();
+    }
 
     private async void DeleteLocation_Clicked(object sender, EventArgs e)
     {
-		var response = await DisplayAlert("Confirm Deletion", "Are you sure you would like to delete this location?", "Yes", "No");
-		if (response)
-		{
-			// Delete Location
-			if (BindingContext is LocationDetailViewModel vm)
-			{
-				var result = await RestService.Instance.DeleteLocationAsync(vm.Location);
-				if (result)
-				{
-					await Shell.Current.GoToAsync("///Locations");
-				}
-				else
-				{
-					await DisplayAlert("API Error", "Failed to delete location.", "Okay");
-				}
-			}
-		}
+
+        if (BindingContext is LocationDetailViewModel vm)
+        {
+            var message = "Are you sure you would like to delete this location?";
+            if (vm.Location.Children.Count > 0)
+                message += $" This will delete {vm.Location.Children.Count} sub-locations.";
+            var response = await DisplayAlert("Confirm Deletion", message, "Yes", "No");
+            if (response)
+            {
+                // Delete Location
+                var result = await RestService.Instance.DeleteLocationAsync(vm.Location);
+                if (result)
+                {
+                    await Shell.Current.GoToAsync("///Locations");
+                }
+                else
+                {
+                    await DisplayAlert("API Error", "Failed to delete location.", "Okay");
+                }
+            }
+        }
     }
 }
